32191962d959722c463f9c1b0736fcebb1ed7573
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-10 21:54:50 +1000

    Function parameters and arguments
    How to pass values (arguments) into functions using parameters.

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/functions.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/functions.js
@@ -15,3 +15,21 @@ function greetings() {
 // Calling or invoking a function
 greetings(); // Outputs the string "Hello there! Welome." onto the console.
 
+/* 
+
+  Passing values into functions:
+  
+  Functions can receive values called "arguments". These are usually stored in what are called "parameters".
+  These parameters act as containers, essentially variables, for values to be stored in and used within functions.
+  
+  Parameters allow more dynamic values to be inserted into functions for function-specific operations, making your code more dynamic.
+
+*/
+
+// Function declaration with parameters
+function greetings(name) {
+  console.log(`Hello ${name}! Welcome aboard.`);
+}
+
+// Invoking a function and passing arguments to it
+greetings("Chris");
