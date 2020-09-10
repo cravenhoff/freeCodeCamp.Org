c13c47dc5470c141ec6fee9137c2afd1db2c6186
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-10 21:48:34 +1000

    Create functions.js
    Creating a simple function and invoking or calling it.

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/functions.js
@@ -0,0 +1,17 @@
+/* FUNCTIONS IN JAVASCRIPT */
+
+/*
+
+  Functions in JavaScript help to create reusable code.
+  Function syntax: function functionName() {};
+  
+*/
+
+// Function declaration
+function greetings() {
+  console.log("Hello there! Welcome.");
+}
+
+// Calling or invoking a function
+greetings(); // Outputs the string "Hello there! Welome." onto the console.
+
