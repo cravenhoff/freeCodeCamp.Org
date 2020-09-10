01d245a0fe0940817c77eb0acd8bc7d16c35695b
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-10 15:28:55 +1000

    Create arrays.js
    Simple arrays and multi-dimensional arrays.

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/arrays.js
@@ -0,0 +1,26 @@
+/* STORING MULTIPLE VALUES IN JAVASCRIPT USING ARRAYS */
+
+/* 
+  Arrays:
+ 
+  Unlike variables, arrays can store and hold multiple values. 
+  The values of arrays can be stored in very basic and simple layers, or can be extended into more complex layers or dimensions.
+  This creates what are called "multi-dimensional arrays" that are essentially nested within one another.
+  
+*/
+
+// Basic: One-Dimensional Arrays
+let shoppingList = ["Apples", "Egg", "Bananas", "Pure Water", "Oranges"];
+
+// Complex: Multi-Dimensions (Nested) Arrays
+let myShoppingList = [
+  ["Apples", 6],
+  ["Egg", 1],
+  ["Bananas", 2],
+  ["Pure Water", 4],
+  ["Oranges", 3]
+];
+
+/* 
+  Accessing Array Data Using Bracket Notation:
+*/
