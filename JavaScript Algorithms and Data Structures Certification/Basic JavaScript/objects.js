3f1994045c6d1ba2ca96bc1227f6014f3bf2992e
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-15 02:05:03 +1000

    Create objects.js
    What are objects - brief overview / summary...

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/objects.js
@@ -0,0 +1,13 @@
+/* OBJECTS */
+
+/*
+
+  Objects:
+  
+  Similar to arrays, objects help store multiple values in a variable-like container. However, unlike arrays, objects do not store data in indexes or in an index-value pair format. Rather objects store data in a more structured manner in what are called "properties". These properties are used to access the object values. Object properties are similar to what we would call "array elements".
+  
+  Objects can be described as a collection of properties. Objects can also be referred to as a type of "associative array", since it deals with string or key-value pairs. Furthermore, objects are directly modelled with real-world objects, and are indivdual entities of their own.
+  
+  Property names in objects can be stored as either strings or numbers. Non-string values are typecast by JavaScript into strings.
+
+*/
