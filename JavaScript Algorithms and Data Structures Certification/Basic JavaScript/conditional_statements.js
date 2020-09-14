3eb6ed3f6778597b33c1561d01db4996d45a289f
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-14 23:57:42 +1000

    Conditional Statements
    Basic summary of what conditional statements are...

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/conditional_statements.js
@@ -0,0 +1,10 @@
+/* CONDITIONAL STATEMENTS */
+
+/*
+
+  Conditional Statements are "control flow statements" that determine the flow or logic of a program based on certain conditions.
+  JavaScript provides the IF, ELSE AND ELSE IF Conditional Statements.
+  
+  These conditional statements return boolean values of either true or false, known as "conditional logic".
+
+*/
