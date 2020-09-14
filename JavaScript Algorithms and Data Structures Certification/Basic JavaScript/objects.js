e841af11cb9f046ca7cf8fe7983fb6a997f60c67
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-15 02:17:12 +1000

    Declaring Objects
    Creating objects  - object declaration using dot notation...

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/objects.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/objects.js
@@ -2,12 +2,27 @@
 
 /*
 
-  Objects:
-  
+  => Objects:
   Similar to arrays, objects help store multiple values in a variable-like container. However, unlike arrays, objects do not store data in indexes or in an index-value pair format. Rather objects store data in a more structured manner in what are called "properties". These properties are used to access the object values. Object properties are similar to what we would call "array elements".
   
-  Objects can be described as a collection of properties. Objects can also be referred to as a type of "associative array", since it deals with string or key-value pairs. Furthermore, objects are directly modelled with real-world objects, and are indivdual entities of their own.
+  Objects can be described as a collection of properties and can also be referred to as a type of "associative array", since it deals with string or key-value pairs. Furthermore, objects are directly modelled with real-world objects, and are indivdual entities of their own.
   
-  Property names in objects can be stored as either strings or numbers. Non-string values are typecast by JavaScript into strings.
+  => Object Properties:
+  Property names in objects can be stored as either strings or numbers. Non-string values are typecast by JavaScript into strings. Property names can be single-worded or can contain multiple words.
+  Depending on which naming method is used, JavaScript has a specific way to call single-word and multi-word properties, with single-word using "dot notation" and multi-word using "bracket notation".
+  
+  => Creating Objects:
+  Objects are created the same way standard variables and arrays are created, using the var or let keyword.
+  There are two different ways to create objects based on whether their "declared" or "initialized".
+  
+  // Declaring Variables
+  let dog = new Object();
+  
+  Ones objects are declared, properties can be assigned to the dynamically using dot notation, like so:
+  dog.name = "Bowi";
+  dog.color = "Goldie Brown";
+  dog.breed = "Golden Retriever";
+  
+  Now dog{} object contains three properties along with their assigned values: name => Bowi, color => Golden Brown, breed => Golder Retriever.
 
 */
