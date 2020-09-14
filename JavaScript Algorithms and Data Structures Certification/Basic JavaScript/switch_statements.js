ce305967899369c1e668cf1591fb3e595458185d
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-15 01:34:04 +1000

    Create switch_statements.js
    Switch statements brief summary and basic sample switch statement...

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/switch_statements.js
@@ -0,0 +1,43 @@
+/* SWITCH STATEMENTS */
+
+/*
+
+  Switch Statements:
+  
+  Switch statements provide a more concise form of conditional flow or logic in a program compared to if else and if else if statements.
+  Switch statements are commonly used in situations that require multiple conditions to be tested.
+  Important to note that switch statments use strict equality when checking a condition against a value.
+  
+  Switch statements provide two structures: switch declaration with value to compare against, and cases (or case statements) - basically conditions to check.
+  Each case in a switch statement can do things like create or assign an existing variable to a value, or perform some arithmetic operation.
+  Switch statements are exited using the BREAK Statement / Keyword at the end of a case declaration. Without the BREAK Statement, JS continues on to run any subsequent case statements until it reachs the next immediate BREAK Statement.
+  
+  Similar to if else statements that provide a default option when no particular condition is being met, switch statements use the DEFAULT Statement to set the default condition to execute when all others fail.
+
+*/
+
+// Switch Statement
+let team = "";
+function checkRank(rank) {
+  switch (rank) {
+    case 0:
+      team = "Alpha";
+      break;
+      
+    case 1:
+      team = "Delta";
+      break;
+      
+    case 2:
+      team = "Charlie";
+      break;
+      
+    case 3:
+      team = "Bravo";
+      break;
+  }
+  
+  return team;
+}
+
+checkRank(3); // Outputs "Bravo".
