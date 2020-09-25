a108592f055bed294d70397db5ff4f37e62e6b13
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-25 15:07:24 +1000

    While Loops
    How to work with while loops.

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/loops.js
@@ -0,0 +1,17 @@
+/* LOOPS IN JAVASCRIPT */
+
+/*
+
+  => While Loops
+  While loops run or execute only "while" the condition is TRUE. The loop is skipped (not executed) when the condition is FALSE.
+  While loops, and loops in general, are essentially IF Statements (Conditional Statements), but instead of using multiple lines of conditional statements, loops help clean-up code and achieve the same thing with just one comman/construct with a few lines of code.
+
+*/
+
+// Create a while loop to print out the numbers 1 through to 5.
+let counter = 1;
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
