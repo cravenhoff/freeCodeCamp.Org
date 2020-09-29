895af486452d72bd1789eb3376408df7b5c68d42
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-30 00:26:39 +1000

    Documentation: reverseString()
    Added appropriate commenting for the recursive function.

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/reverse_string_recursion.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/reverse_string_recursion.js
@@ -15,21 +15,24 @@ let reverseString = str => {
 	if (stringLength === 0) {
 		return "You've run out of characters!";
 	}
-    
-    
+    	
+	// Saves the last character of the string
 	let strChar = str[stringLength - 1]
 
 	let newStr = "";
-	newStr += str.slice(counter, stringLength - 1);
-
+	newStr += str.slice(counter, stringLength - 1); // Returns a new string minus the last character
+	
+	// Push the last string character into a new array
 	reversedChar.push(strChar);
-
+	
+	// Conver the array into a string
 	reversedStr = reversedChar.toString();
-
+	
+	// Removes (replaces) the commas in the string returned from the toString() Method
 	finalStr = reversedStr.replace(/,/g, "");
 
 	console.log(finalStr);
-    return reverseString(newStr);
+    	return reverseString(newStr);
 }
 
 console.log(reverseString("freeCodeCamp")); // Prints out the string in reverse "pmaCedoCeerf"
