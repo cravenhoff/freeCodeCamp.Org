c1ac952d4119c06e2837279b23161683a825c3f8
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-29 23:28:44 +1000

    Create profile_lookup.js
    Added the program instructions.

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/profile_lookup.js
@@ -0,0 +1,20 @@
+/* 
+
+  Create a function that looks up a list of contacts and profiles of individuals, and checks if the provided name exists in the contact and whether or not the particular contact contains the property passed into the function.
+  
+  INSTRUCTIONS:
+  
+  1. The function should accept two parameters => contact/profile name, and property.
+  
+  2. The function should be able to "lookup" a contact list or profile, to find the following:
+    a. Name passed into the function - the name will reference the "first name" of the contact,
+    b. Property passed into the function - the property linked to the name argument.
+    
+  3. If the name is found in the contact, the function should proceed to check if the property provided is linked to the name found.
+    a. If this is TRUE, the function should return the property value
+    b. Else the function should return the string "No such property"
+    
+  4. If the name is NOT FOUND in the contact, the function should instead return "No such contact".
+
+*/
+
