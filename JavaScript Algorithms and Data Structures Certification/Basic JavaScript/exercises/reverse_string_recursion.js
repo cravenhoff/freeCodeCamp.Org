25bf00faad347a73c815d8ff7f97a28e49d9e91f
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-30 00:18:48 +1000

    reverseString() Recursion
    Added the reverseString() recursive function.

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/reverse_string_recursion.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/reverse_string_recursion.js
@@ -2,3 +2,34 @@
   FREECODECAMP EXERCISE:
   1. Write a program that reverses a string using recursion. Given the string "freeCodeCamp" your program should return "pmaCedoCeerf".
 */
+
+let counter = 0;
+let reversedChar = [];
+let reversedStr = "";
+let finalStr = "";
+
+let reverseString = str => {
+	let stringLength = str.length;
+
+	// Define base camp
+	if (stringLength === 0) {
+		return "You've run out of characters!";
+	}
+    
+    
+	let strChar = str[stringLength - 1]
+
+	let newStr = "";
+	newStr += str.slice(counter, stringLength - 1);
+
+	reversedChar.push(strChar);
+
+	reversedStr = reversedChar.toString();
+
+	finalStr = reversedStr.replace(/,/g, "");
+
+	console.log(finalStr);
+    return reverseString(newStr);
+}
+
+console.log(reverseString("freeCodeCamp"));
