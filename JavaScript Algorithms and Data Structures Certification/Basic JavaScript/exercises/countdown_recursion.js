864f43d0989fe0ffc05812e4129a4a1e5d62be1e
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-30 21:54:04 +1000

    Script: countdown() Function
    Coded in the recursive function script to take in a number argument, and decrement it after every function call until the number reaches zero.

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/countdown_recursion.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/countdown_recursion.js
@@ -6,3 +6,17 @@
   Create a recursive function that takes in one argument, a number, and counts down from that number until the function reaches zero.
 
 */
+
+// Create recursive function
+let countdown = number => {
+  // Define base camp
+  if(number === 0) {
+    return;
+  }
+  
+  console.log(number);
+  return countdown(number - 1);
+}
+
+// Call the function to execute
+console.log(countdown(5)); // Prints out the numbers from 5 to 0.
