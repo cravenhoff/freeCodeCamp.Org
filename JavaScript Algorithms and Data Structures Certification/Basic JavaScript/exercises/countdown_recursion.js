d59d22f4fec2859dc9b10c21e4c478f1fcc4da51
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-30 21:50:18 +1000

    Create countdown_recursion.js

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/countdown_recursion.js
@@ -0,0 +1,8 @@
+/* RECURSIVE ALGORITHM THAT COUNTS DOWN FROM A GIVEN NUMBER */
+
+/*
+
+  Recursion Exercise:
+  Create a recursive function that takes in one argument, a number, and counts down from that number until the function reaches zero.
+
+*/
