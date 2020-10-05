f2c443670d2003e64faa741ad2b7a7b584bb9c28
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-05 23:51:34 +1000

    Update range_of_numbers_recursion.js

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/range_of_numbers_recursion.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/exercises/range_of_numbers_recursion.js
@@ -1,4 +1,13 @@
 /*
   FREECODECAMP EXERCISE:
   1. Use recursions to create a function that returns an array of numbers from the given starting number and ends at the specified ending integer.
+  
+  INSTRUCTIONS:
+  => The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter.
+  
+  => The starting number will always be less than or equal to the ending number.
+  
+  => Your function must use recursion by calling itself and not use loops of any kind.
+  
+  => It should also work for cases where both startNum and endNum are the same.
 */
